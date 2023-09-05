be58105afa803cc84d866a217638ef8d9b8db48a
Nikita Yurchenko <20leviafunny03@mail.ru>
2023-09-05 21:38:42 +0300

    Создал функции-заглушки представления (views) для регистрации, авторизации и главной страницы. Настроил urls для приложений (lentaSowork, authorization).

--- sowork/authorization/views.py
+++ sowork/authorization/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def register(request):
+    return HttpResponse('Страница регистрации')
+
+
+def login(request):
+    return HttpResponse('Страница авторизации')
 # Create your views here.
--- sowork/lentaSowork/views.py
+++ sowork/lentaSowork/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def lenta(request):
+    return HttpResponse('Лента')
 # Create your views here.
