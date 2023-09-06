76d5e5ef4badc84e22bc39006e9723153907f68d
Alexey Kuznetsov <djamperkuz@yandex.ru>
2023-09-06 16:32:00 +0300

    fix bug in view (register, login)

--- sowork/authorization/views.py
+++ sowork/authorization/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 
 def register(request):
-    return render(request, 'authorization/register')
+    return render(request, 'authorization/register.html')
 
 
 def login(request):
-    return render(request, 'authorization/login')
+    return render(request, 'authorization/login.html')
 # Create your views here.
