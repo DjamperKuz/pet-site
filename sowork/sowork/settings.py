44a194ce5c2295b914f94a7eff8fa339b4900e33
Michael <thesuspect182@gmail.com>
2023-09-08 15:11:33 +0300

    Создал приложение мессенджера

--- sowork/sowork/settings.py
+++ sowork/sowork/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authorization.apps.AuthorizationConfig',
     'lentaSowork.apps.LentasoworkConfig',
+    'messenger.apps.MessengerConfig'
 ]
 
 MIDDLEWARE = [
