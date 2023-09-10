22eeced2da39ba09f9834ac9c68f159f99bf4401
Michael <thesuspect182@gmail.com>
2023-09-10 15:38:31 +0300

    Создал папку static, перенёс туда пресеты

--- sowork/messenger/urls.py
+++ sowork/messenger/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('messenger', views.messenger_home, name='messenger_home'),
+    path('messenger_api', views.messenger_api),
 ]
