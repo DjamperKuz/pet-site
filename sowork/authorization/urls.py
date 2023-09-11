5907930ec55929da40decc9d8e4051d3d852f459
Alexey Kuznetsov <djamperkuz@yandex.ru>
2023-09-11 16:12:49 +0300

    Обновил авторизацию и регистрацию, теперь сразу перенаправляет на страницу профиля пользователя

--- sowork/authorization/urls.py
+++ sowork/authorization/urls.py
@@ -3,6 +3,6 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path('login/', LoginUser.as_view(), name='login'),
+    path('', LoginUser.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
 ]
