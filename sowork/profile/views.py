bbdb232f29799309ee8f34451ab5d032c3d515c4
Alexey Kuznetsov <djamperkuz@yandex.ru>
2023-09-12 12:29:38 +0300

    Добавил переменную для получения логина авторизованного пользователя

--- sowork/profile/views.py
+++ sowork/profile/views.py
@@ -7,8 +7,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 @login_required(login_url='/')
 def profile(request, username):
     post = get_object_or_404(User, username=username)
+    current_user = request.user
 
-    return render(request, 'profile/user_profile.html', context={'username': post})
+    return render(request, 'profile/user_profile.html', context={'username': post, 'current_user': current_user})
 
 
 def logout_user(request):
