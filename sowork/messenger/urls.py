b1de63e74d754655c6c98721dcd4a94df9b9f356
Michael <thesuspect182@gmail.com>
2023-09-12 15:16:16 +0300

    Сделал возможным открывать чат с указанным в адресной строке пользователем

--- sowork/messenger/urls.py
+++ sowork/messenger/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('chats', views.chat_list, name='chat_list'),
     path('chats/<int:chat_id>/', views.chat_detail, name='chat_detail'),
+    path('chats/<str:username>/', views.open_or_create_chat, name='open_or_create_chat'),
 ]
--- sowork/messenger/views.py
+++ sowork/messenger/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Chat, Message
 from .forms import MessageForm
 
@@ -23,3 +24,19 @@ def chat_detail(request, chat_id):
         form = MessageForm()
 
     return render(request, 'messenger/chat_detail.html', {'chat': chat, 'messages': messages, 'form': form})
+
+
+def open_or_create_chat(request, username):
+    # найти пользователя с указанным username
+    other_user = get_object_or_404(User, username=username)
+
+    # проверить, существует ли указанный чат
+    chat = Chat.objects.filter(participants=request.user).filter(participants=other_user).first()
+
+    if chat is None:
+        # если чат не существует, создаём новый
+        chat = Chat.objects.create()
+        chat.participants.add(request.user, other_user)
+
+    # перенаправляем пользователя на страницу чата
+    return redirect('chat_detail', chat_id=chat.id)
