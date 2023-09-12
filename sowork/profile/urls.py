cf59c0f6ccbf75b80ce42b5ca72482bb4956e0ba
Alexey Kuznetsov <djamperkuz@yandex.ru>
2023-09-12 11:57:15 +0300

    Добавил кнопку выхода из аккаунта, сделал перенаправление с главной страницы, если пользователь не авторизован

--- sowork/profile/urls.py
+++ sowork/profile/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('profile/<slug:username>', views.profile, name='user_profile'),
+    path('profile/<slug:username>/', views.profile, name='user_profile'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- sowork/profile/views.py
+++ sowork/profile/views.py
@@ -1,9 +1,18 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 
+@login_required(login_url='/')
 def profile(request, username):
     post = get_object_or_404(User, username=username)
 
     return render(request, 'profile/user_profile.html', context={'username': post})
+
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('login')
 
